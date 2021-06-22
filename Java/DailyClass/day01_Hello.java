15657c4ad2be434e8cd73c2bae04a5efc962ea63
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-22 18:09:59 +0900

    Add files via upload

---
+++ Java/DailyClass/day01_Hello.java
@@ -0,0 +1,13 @@
+package start;	//항상 맨 위에는 package 이름이 와야한다.
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// main 함수의 처리 내용을 넣어보자.
+		// syso, sysout + ctrl + spacebar
+		System.out.println("Welcome.");
+		System.out.print("Nice to meet you.");
+		
+	}
+
+}
---
+++ Java/DailyClass/day01_Hi.java
@@ -0,0 +1,13 @@
+package start;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		System.out.println("자바에는");
+		System.out.println("점이");
+		System.out.println("참 많네요");
+
+
+	}
+
+}
