2c86f58aee73e101f9a7071f5a1149e4dff2d4d5
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-08-07 23:42:48 +0900

    Update CartDAO.java

--- Java/Project1_bairbnb/CartDAO.java
+++ Java/Project1_bairbnb/CartDAO.java
@@ -1,4 +1,4 @@
-// 2021.07.29 updated
+// 2021.07.30 updated
 
 package cart;
 
@@ -10,23 +10,39 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class CartDAO {
-	
+	Connection con;
+
+	public CartDAO() {
+
+		try {
+			//Class.forName("com.mysql.cj.jdbc.Driver");	//mac
+			Class.forName("com.mysql.jdbc.Driver");			//window
+			
+			String url = "jdbc:mysql://localhost:3306/bairbnb";
+			//String url = "jdbc:mysql://localhost:3306/bairbnb?useUnicode=true&characterEncoding=utf8"; //순선
+			String user = "root";
+			
+			String password = "1234";						//주현,순선,이랑
+			//String password = "cnzk0531";					//혜윤
+			//String password = "atempause26"; 				//성민
+			con = DriverManager.getConnection(url, user, password);
+			System.out.println("2. BAIRBNB db연결 성공!!!");
+		} catch (ClassNotFoundException e) {
+			System.out.println("1번에러>> 드라이버없음");
+		} catch (SQLException e) {
+			System.out.println("2~4번에러>> DB관련 에러");
+		}
+
+	}
+
 	public int cartCreate(CartDTO cart) {
 		int result = 0;
 		try {
-			// 1. jdbc connector설정
-			Class.forName("com.mysql.jdbc.Driver");
-			System.out.println("1. connector연결 성공!!!");
-			// 2. java에서 db로 연결
-			String url = "jdbc:mysql://localhost:3306/bairbnb";
-			String username = "root";
-			String password = "1234";
-			Connection con = DriverManager.getConnection(url, username, password);
-			System.out.println("2. BAIRBNB db연결 성공!!!");
+
 			// 3. sql문을 만든다.
 			String sql = "insert into cart values (null, ?, ?, ?, ?, ?)";
 			PreparedStatement ps = con.prepareStatement(sql);
-			ps.setString(1, cart.getBuyerid());
+			ps.setString(1, cart.getMemid());
 			ps.setInt(2, cart.getProid());
 			ps.setString(3, cart.getCcheckin());
 			ps.setString(4, cart.getCcheckout());
@@ -36,66 +52,75 @@ public class CartDAO {
 			// 4. sql문을 mysql로 전송한다.
 			result = ps.executeUpdate();
 			System.out.println("4. sql문 전송 전송");
-			System.out.println(result);	//확인용
-		} catch (ClassNotFoundException e) {
-			System.out.println("1번에러>> 드라이버없음");
+			System.out.println(result); // 확인용
 		} catch (SQLException e) {
 			System.out.println("2~4번에러>> DB관련 에러");
 		}
 		return result;
 	}
-	
-	public ArrayList<CartDTO> cartRead(String buyerid) {
-		
+
+	// 전체검색
+	public ArrayList<CartDTO> cartRead(String memid) {
+
 		ArrayList<CartDTO> result = new ArrayList<CartDTO>();
-		
+
 		try {
-			// 1. jdbc connector설정
-			Class.forName("com.mysql.jdbc.Driver");
-			System.out.println("1. connector연결 성공!!!");
-			// 2. java에서 db로 연결
-			String url = "jdbc:mysql://localhost:3306/bairbnb";
-			String username = "root";
-			String password = "1234";
-			Connection con = DriverManager.getConnection(url, username, password);
-			System.out.println("2. BAIRBNB db연결 성공!!!");
 			// 3. sql문을 만든다.
-			String sql = "select * from cart where buyerid = ? order by cartidx asc";
+			String sql = "select * from cart where memid = ? order by ccheckin asc";
 			PreparedStatement ps = con.prepareStatement(sql);
-			ps.setString(1, buyerid);
+			ps.setString(1, memid);
 			System.out.println("3. SQL문 생성 완료");
-			
+
 			// 4. sql문을 mysql로 전송한다.
 			ResultSet rs = ps.executeQuery();
 			System.out.println("4. sql문 전송 전송");
-						
+
 			while (rs.next()) {
-				result.add(new CartDTO(rs.getInt(1), buyerid, rs.getInt(3), rs.getString(4), rs.getString(5), rs.getInt(6)));
+				result.add(new CartDTO(rs.getInt(1), memid, rs.getInt(3), rs.getString(4), rs.getString(5),
+						rs.getInt(6)));
 				System.out.println("read완료");
 			}
-									
-		} catch (ClassNotFoundException e) {
-			System.out.println("1번에러>> 드라이버없음");
+
 		} catch (SQLException e) {
 			System.out.println("2~4번에러>> DB관련 에러");
 		}
-		
+
 		return result;
-		
 	}
-		
+
+	// cartidx로 검색
+	public CartDTO cartRead(int cartidx) {
+
+		CartDTO result = null;
+
+		try {
+			// 3. sql문을 만든다.
+			String sql = "select * from cart where cartidx = ?";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1, cartidx);
+			System.out.println("3. SQL문 생성 완료");
+
+			// 4. sql문을 mysql로 전송한다.
+			ResultSet rs = ps.executeQuery();
+			System.out.println("4. sql문 전송");
+
+			if (rs.next()) {
+				result = new CartDTO(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getString(4), rs.getString(5),
+						rs.getInt(6));
+				System.out.println("read완료");
+			}
+
+		} catch (SQLException e) {
+			System.out.println("2~4번에러>> DB관련 에러");
+		}
+
+		return result;
+
+	}
+
 	public int cartUpdate(CartDTO cart) {
 		int result = 0;
 		try {
-			// 1. jdbc connector설정
-			Class.forName("com.mysql.jdbc.Driver");
-			System.out.println("1. connector연결 성공!!!");
-			// 2. java에서 db로 연결
-			String url = "jdbc:mysql://localhost:3306/bairbnb";
-			String username = "root";
-			String password = "1234";
-			Connection con = DriverManager.getConnection(url, username, password);
-			System.out.println("2. BAIRBNB db연결 성공!!!");
 			// 3. sql문을 만든다.
 			String sql = "update cart set ccheckin = ?, ccheckout = ?, cartprice = ? where cartidx = ?";
 			PreparedStatement ps = con.prepareStatement(sql);
@@ -108,78 +133,31 @@ public class CartDAO {
 			// 4. sql문을 mysql로 전송한다.
 			result = ps.executeUpdate();
 			System.out.println("4. sql문 전송 전송");
-			System.out.println(result);	//확인용
-		} catch (ClassNotFoundException e) {
-			System.out.println("1번에러>> 드라이버없음");
+			System.out.println(result); // 확인용
 		} catch (SQLException e) {
 			System.out.println("2~4번에러>> DB관련 에러");
 		}
 		return result;
 	}
-	
-	
-	//결제 시 카트 초기화
-	public int cartDelete(String buyerid) {
-		
+
+	// 장바구니 내 카트 선택 삭제
+	public int cartDelete(int cartidx) {
 		int result = 0;
 		try {
-			// 1. jdbc connector설정
-			Class.forName("com.mysql.jdbc.Driver");
-			System.out.println("1. connector연결 성공!!!");
-			// 2. java에서 db로 연결
-			String url = "jdbc:mysql://localhost:3306/bairbnb";
-			String username = "root";
-			String password = "1234";
-			Connection con = DriverManager.getConnection(url, username, password);
-			System.out.println("2. BAIRBNB db연결 성공!!!");
 			// 3. sql문을 만든다.
-			String sql = "delete from cart where buyerid = ?";
+			String sql = "delete from cart where cartidx = ?";
 			PreparedStatement ps = con.prepareStatement(sql);
-			ps.setString(1, buyerid);
+			ps.setInt(1, cartidx);
 			System.out.println("3. SQL문 생성 완료");
-			
+
 			// 4. sql문을 mysql로 전송한다.
 			result = ps.executeUpdate();
-			System.out.println("4. sql문 전송");
-											
-		} catch (ClassNotFoundException e) {
-			System.out.println("1번에러>> 드라이버없음");
+			System.out.println("4. sql문 전송 전송");
+
 		} catch (SQLException e) {
 			System.out.println("2~4번에러>> DB관련 에러");
 		}
 		return result;
 	}
-	
-	//장바구니 내 카트 선택 삭제
-		public int cartDelete(int cartidx) {
-			int result = 0;
-			try {
-				// 1. jdbc connector설정
-				Class.forName("com.mysql.jdbc.Driver");
-				System.out.println("1. connector연결 성공!!!");
-				// 2. java에서 db로 연결
-				String url = "jdbc:mysql://localhost:3306/bairbnb";
-				String username = "root";
-				String password = "1234";
-				Connection con = DriverManager.getConnection(url, username, password);
-				System.out.println("2. BAIRBNB db연결 성공!!!");
-				// 3. sql문을 만든다.
-				String sql = "delete from cart where cartidx = ?";
-				PreparedStatement ps = con.prepareStatement(sql);
-				ps.setInt(1, cartidx);
-				System.out.println("3. SQL문 생성 완료");
-				
-				// 4. sql문을 mysql로 전송한다.
-				result = ps.executeUpdate();
-				System.out.println("4. sql문 전송 전송");
-												
-			} catch (ClassNotFoundException e) {
-				System.out.println("1번에러>> 드라이버없음");
-			} catch (SQLException e) {
-				System.out.println("2~4번에러>> DB관련 에러");
-			}
-			return result;
-		}
-	
 
 }
