e239e36420426938fd4fe9c9ea63f5eb7fd9a091
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-08-30 12:13:47 +0900

    Create CastingDerive.java

---
+++ Java/2_Casting/CastingDerive.java
@@ -0,0 +1,21 @@
+package com.mega.mvc08;
+
+public class CastingDerive {
+
+	public static void main(String[] args) {
+		
+		//파생된 데이터, 참조형 변수
+		//형변환 (casting, 캐스팅)
+		//참조형 형변환은 "상속관계에 있는 클래스간" 가능
+		
+		Stationary s = new Stationary();	//부모, 수퍼, 큰
+		BPPen b = new BPPen();				//자식, 서브, 작
+		
+		//클래스 간의 크기는 개념을 가지고 큰 지/작은 지를 판별
+		
+		s = b;			//자동형변환: 큰 <--- 작
+		//b = s;		//불가능
+		b = (BPPen)s;	//강제형변환: 작 <--- 큰
+	}
+
+}
