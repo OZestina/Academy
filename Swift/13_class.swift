82ab560f34ee1b80333f9e62e4ca9ff9ac989171
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-11-09 16:21:18 +0900

    Update 13_class.swift

--- Swift/13_class.swift
+++ Swift/13_class.swift
@@ -9,10 +9,13 @@ import Foundation
 
 //Swift는 함수형 언어여서
 //하나의 파일에 클래스를 여러개 생성해도 괜찮다!
-//하나의 파일에 클래스를 선언해서 전역변수를 공유할 수도 있다
-//(전역변수를 클래스 밖에 선언 가능!)
+
+//클래스 밖에 선언한 초글로벌변수(정확한 이름 아님 주의)는
+//해당 파일의 클래스들 뿐 아니라
+//패키지 내 다른 클래스에서 바로 적용이 가능하다
 
 //같은 패키지에 있는 파일끼리는 import 필요없당!
+
 
 class BasicCal{
     
@@ -20,7 +23,21 @@ class BasicCal{
     
     func prints() {
         print("멤버변수값: ",price)
+        print(value)                //아래 다른파일에서 선언된 초 글로벌변수 바로 사용
     }
-    
-    
 }
+
+
+
+
+
+//========[다른 파일]=============================
+
+import UIKit
+
+//초글로벌변수 선언: 클래스 밖에 선언
+var total2: UILabel?
+var value = 100
+
+class ViewController: UIViewController {
+    
